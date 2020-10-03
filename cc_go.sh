828fdef90d4163aabbf40471e6a8d8a96aa79ed5
5m17h <dorleone@163.com>
2020-10-03 12:45:06 +0800

    安装
    安装

---
+++ cc_go.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+yum install epel-release -y
+yum install nload -y
+yum -y groupinstall 'Development Tools'
+yum -y install openssl-devel git
+git clone https://github.com/wg/wrk.git
+cd wrk
+make
+cp wrk /usr/local/bin/

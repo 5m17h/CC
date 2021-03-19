0f4f8d43727b9876780758f29a8058914a9db7ed
5m17h <dorleone@163.com>
2021-03-19 16:49:40 +0800

    Update cc_go.sh
    添加了screen

--- cc_go.sh
+++ cc_go.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 yum install epel-release -y
 yum install nload -y
+yum install screen -y
 yum -y groupinstall 'Development Tools'
 yum -y install openssl-devel git
 git clone https://github.com/wg/wrk.git
